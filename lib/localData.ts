08bcaea2e67ec8b1e77bfffae6bfb625034ddad2
Novia Chao <noviachao@Novias-MacBook-Pro.local>
2024-12-31 03:26:46 +0100

    PNG to JPG

--- lib/localData.ts
+++ lib/localData.ts
@@ -4,36 +4,37 @@ export interface Postcard {
   image_url: string;
 }
 
+// Update paths to include /images/ directory
 export const postcards: Postcard[] = [
   {
     id: '1',
     month: 'January',
-    image_url: '/postcard1.png',
+    image_url: '/postcard1.jpg',
   },
   {
     id: '2',
     month: 'February',
-    image_url: '/postcard2.png',
+    image_url: '/postcard2.jpg',
   },
   {
     id: '3',
     month: 'March',
-    image_url: '/postcard3.png',
+    image_url: '/postcard3.jpg',
   },
   {
     id: '4',
     month: 'April',
-    image_url: '/postcard4.png',
+    image_url: '/postcard4.jpg',
   },
   {
     id: '5',
     month: 'May',
-    image_url: '/postcard5.png',
+    image_url: '/postcard5.jpg',
   },
   {
     id: '6',
     month: 'June',
-    image_url: '/postcard6.png',
+    image_url: '/postcard6.jpg',
   },
 ];
 
