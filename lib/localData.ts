f16ebaac6557f303f8050a3d9f8239de72e9f0f0
Novia Chao <noviachao9@gmail.com>
2025-01-04 00:08:44 +0000

    change jpg to JPG

--- lib/localData.ts
+++ lib/localData.ts
@@ -9,32 +9,32 @@ export const postcards: Postcard[] = [
   {
     id: '1',
     month: 'January',
-    image_url: '/postcard1.jpg',
+    image_url: '/postcard1.JPG',
   },
   {
     id: '2',
     month: 'February',
-    image_url: '/postcard2.jpg',
+    image_url: '/postcard2.JPG',
   },
   {
     id: '3',
     month: 'March',
-    image_url: '/postcard3.jpg',
+    image_url: '/postcard3.JPG',
   },
   {
     id: '4',
     month: 'April',
-    image_url: '/postcard4.jpg',
+    image_url: '/postcard4.JPG',
   },
   {
     id: '5',
     month: 'May',
-    image_url: '/postcard5.jpg',
+    image_url: '/postcard5.JPG',
   },
   {
     id: '6',
     month: 'June',
-    image_url: '/postcard6.jpg',
+    image_url: '/postcard6.JPG',
   },
 ];
 
